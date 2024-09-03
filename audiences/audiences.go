0380c48638c6adae06da48c554a3ff05c16201e0
eric rodefeld <eric.rodefeld@cognitivespace.com>
2024-09-03 19:00:32 -0400

    cleanup

--- audiences/audiences.go
+++ audiences/audiences.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+    "fmt"
     "oracle.com/soothsayers"
     "net/http"
     "io"
@@ -10,43 +11,50 @@ import (
 
 func main() {    
     http.HandleFunc("/attend", AttendAudience)
+    log.Print("Running server")
     http.ListenAndServe(":7777", nil)
 }
 
-
 func AttendAudience(w http.ResponseWriter, req *http.Request) {
     log.SetPrefix("audiences: ")
-
+    
     // Get body of request
-
     body, err := io.ReadAll(req.Body)
     if err != nil {
         log.Print(err)
     }
     req.Body.Close()
+    
+    log.Print(fmt.Sprintf(string(body[:])))
+    // Convert the input data into a string
     var s string
-    log.Print(string(body[:]))
+    
     err = json.Unmarshal(body, &s)
     if err != nil {
         log.Print(err)
     }
     
-    var fr soothsayers.FeasibilityRequest
-    err = json.Unmarshal([]byte(s), &fr)
-    if err != nil {
-        log.Print(err)
-    }
+    var freq soothsayers.FeasibilityRequest
+    // Convert json string body to internal struct 
+    err = json.Unmarshal([]byte(s), &freq)
     
     // Attend to request
-    feasibilityData, err := soothsayers.Attend(fr)
+    fres, err := soothsayers.Attend(freq)
+
+    // log is global, reset prefix after soothsayers is complete
+    log.SetPrefix("audiences: ")
     if err != nil {
         log.Print(err)
     }
-    log.Print(fr)
 
-    // If no error was returned, print the returned feasibilityData
+    // If no error was returned, print the returned ConfidenceScore
     // to the console.
-    log.Print(feasibilityData)
+    log.Print(fmt.Sprintf("[%v|%s] Ran feasibility! Confidence score: %v", fres.FeasibilityFinderId, fres.Id, fres.ConfidenceScore))  
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)    
+    json.NewEncoder(w).Encode(fres)
+
 }
 
 
--- soothsayers/soothsayers.go
+++ soothsayers/soothsayers.go
@@ -7,46 +7,113 @@ import (
     "math/rand"
 )
 
+type Metadata struct {
+    Constellation string `json:"constellation"`
+}
+
+type Rules struct {
+    CloudCoveragePct int `json:"cloud_coverage_pct"`
+    AISResolutionMaxCm int `json:"is_resolution_max_cm"`
+    AISResolutionMinCm int `json:"ais_resolution_min_cm`
+    EOResolutionMaxCm int `json:"eo_resolution_max_cm"`
+    EOResolutionMinCm int `json:"eo_resolution_min_cm`
+    HSIResolutionMaxCm int `json:"hsi_resolution_max_cm"`
+    HSIResolutionMinCm int `json:"hsi_resolution_min_cm`
+    RFResolutionMaxCm int `json:"rf_resolution_max_cm"`
+    RFResolutionMinCm int `json:"rf_resolution_min_cm`
+    SARResolutionMaxCm int `json:"sar_resolution_max_cm"`
+    SARResolutionMixCm int `json:"sar_resolution_min_cm"`  
+}
+
 type FeasibilityRequest struct {
-    feasibilityFinderId int `json:"feasibility_finder_id"`
-    cloudCoveragePct int `json:"cloud_coverage_pct"`
-    startDate time.Time `json:"start_date"`
-    endDate time.Time `json:"end_date"`
-    geometry string `json:"geometry"`
-    aisResolutionMaxCm int `json:"is_resolution_max_cm"`
-    aisResolutionMinCm int `json:"ais_resolution_min_cm`
-    eoResolutionMaxCm int `json:"eo_resolution_max_cm"`
-    eoResolutionMinCm int `json:"eo_resolution_min_cm`
-    hsiResolutionMaxCm int `json:"hsi_resolution_max_cm"`
-    hsiResolutionMinCm int `json:"hsi_resolution_min_cm`
-    rfResolutionMaxCm int `json:"rf_resolution_max_cm"`
-    rfResolutionMinCm int `json:"rf_resolution_min_cm`
-    sarResolutionMaxCm int `json:"sar_resolution_max_cm"`
-    sarResolutionMixCm int `json:"sar_resolution_min_cm"`
-    
+    FeasibilityFinderId int `json:"feasibility_finder_id"`
+    StartDate time.Time `json:"start_date"`
+    EndDate time.Time `json:"end_date"`
+    Geometry string `json:"geometry"`
+    Rules Rules `json:"rules"`
 }
-const feasibilityResultScoreMultiplier = 100
+
+type FeasibilityResult struct {
+    Id string `json:"id"`
+    FeasibilityFinderId int `json:"feasibility_finder_id"`
+    ConfidenceScore float32 `json:"confidence_score"`
+    StartDate time.Time `json:"start_date"`
+    EndDate time.Time `json:"end_date"`
+    Metadata Metadata `json:"metadata"`
+}
+
+const resultScoreMultiplier float32 = 100
+const minimumFutureSeconds int = 120
 
 
-func Attend(fr FeasibilityRequest) (float32, error) {
+func Attend(freq FeasibilityRequest) (FeasibilityResult, error) {
     log.SetPrefix("soothsayers: ")
-    log.SetFlags(0)
+    // Create a random ID for this request
+    id, err := randomUUID()
+    if err != nil {
+        log.Print(fmt.Sprintf("[%v|-1]: Failed randomUUID! : %s", freq.FeasibilityFinderId,err))  
+    }
+    log.Print(fmt.Sprintf("[%v|%s]: Attending to request", freq.FeasibilityFinderId, id))  
 
-    log.Print(fmt.Sprintf("Attending to %v", fr.feasibilityFinderId))  
-    feasilbilityResultData, err := GetFeasibility(fr)
-    return feasilbilityResultData, err
+    
+
+    endDate, err := randomFutureTime(freq.EndDate)
+    if err != nil {
+        log.Print(fmt.Sprintf("[%v|%s]: Failed randomFutureTime endDate! : %s", freq.FeasibilityFinderId, id, err))  
+    }
+    startDate, err := randomFutureTime(endDate)
+    if err != nil {
+        log.Print(fmt.Sprintf("[%v|%s]: Failed randomFutureTime startDate! : %s", freq.FeasibilityFinderId,  id, err))  
+    }
+    confidenceScore := randomConfidenceScore()
+    metadata := randomMetadata()
+
+    fres := FeasibilityResult{
+        Id: id,
+        FeasibilityFinderId: freq.FeasibilityFinderId,
+        ConfidenceScore: confidenceScore,
+        Metadata: metadata,
+        StartDate: startDate,
+        EndDate: endDate,
+    }
+    return fres, nil
 }
 
 
+func randomConfidenceScore() float32{
+    return rand.Float32() * resultScoreMultiplier
+}
 
+func randomFutureTime(maxFuture time.Time) (time.Time, error) {
+    nowTime := time.Now()
+    timeDeltaSeconds := int(maxFuture.Sub(nowTime).Seconds())
+    if timeDeltaSeconds <= 0 {
+        return maxFuture, fmt.Errorf("time provided was in the past: %v %v",maxFuture, timeDeltaSeconds)
+    }
+    secondsToAddN := int(minimumFutureSeconds + timeDeltaSeconds)
+    secondsToAdd := rand.Intn(secondsToAddN)
+    durationSeconds := time.Second * time.Duration(secondsToAdd)
+    return nowTime.Add(durationSeconds), nil
+}
 
-func GetFeasibility(fr FeasibilityRequest) (float32, error) {
-    log.Print(fmt.Sprintf("Getting feasibility for %v", fr.feasibilityFinderId))  
-    feasibilityResultScore := rand.Float32() * feasibilityResultScoreMultiplier
-    // add error potential
-    // if feasibilityResultScore < 5 {
-    //     return -1, errors.New(fmt.Sprintf("Feasibility score too low! Failed with feasibilityResultScore %v", feasibilityResultScore))
-    // }
-    return feasibilityResultScore, nil
+func randomMetadata() Metadata{
+    constellationOptions := []string{
+        "Ursa Major",
+        "Orion",
+        "Leo",
+        "Andromeda",
+    } 
+    name := constellationOptions[rand.Intn(len(constellationOptions))]
+    metadata := Metadata{Constellation:name}
+    return metadata
+}
 
+func randomUUID() (string, error){
+    b := make([]byte, 16)
+    _, err := rand.Read(b)
+    if err != nil {
+        log.Print(err)
+    }
+    uuid := fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+    return uuid, err  
 }
